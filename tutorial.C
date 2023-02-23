2f0d6b6235d24f25c18ce07aaefa844a9536f246
Armaankhan90 <111609595+Armaankhan90@users.noreply.github.com>
2023-02-23 10:37:28 +0530

    Update and rename README.md to tutorial.C

---
+++ tutorial.C
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main()
+{
+  int x,y;
+  printf("Enter any number:);
+  scanf("%d",&x);
+   for(y=1;y<=10;y++");
+       {
+         printf("%d*%d=%d\n",x,y,x*y);
+       }
+       return 0
+       }
